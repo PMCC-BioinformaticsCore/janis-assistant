26d1bd373d22ea403744f46de2a66fcf7d9e5c27
Michael Franklin <michael@illusional.net>
2019-05-20 11:24:46 +1000

    Prepare move for CLI stubs

--- shepherd/__init__.py
+++ shepherd/__init__.py
@@ -1,2 +1,45 @@
-from .engines import Cromwell, CWLTool, SyncTask, AsyncTask, CromwellConfiguration
-from .utils.fromjanis import from_janis
+import argparse
+
+# from shepherd.engines import Cromwell, CWLTool, SyncTask, AsyncTask, CromwellConfiguration
+from shepherd.utils.logger import Logger
+
+__version__ = "v0.0.1"
+
+
+def process_args():
+
+    cmds = {
+        "version": do_version,
+        "run": do_run
+    }
+
+    parser = argparse.ArgumentParser(description="Execute a workflow")
+    subparsers = parser.add_subparsers(help="subcommand help", dest="command")
+    parser.add_argument("-d", "--debug", action="store_true")
+
+    parser_version = subparsers.add_parser("version")
+
+    parser_run = subparsers.add_parser("run")
+    parser_run.add_argument("workflow")
+    parser_run.add_argument("-i", "--inputs", help="workflow inputs")
+    parser_run.add_argument("-p", "--tools", help="required dependencies")
+    parser_run.add_argument("-e", "--environment", choices=["pmac"], default="pmac")
+    parser_run.add_argument("-E", "--engine", choices=["cromwell", "cwltool"], default="cromwell")
+
+    args = parser.parse_args()
+    print(args)
+    cmds[args.command](args)
+
+
+def do_version(args):
+    print("v0.0.1")
+
+
+def do_run(args):
+    Logger.info("Run the shepherd-shepherd with the CommandLine arguments")
+    print(args)
+    raise NotImplementedError("This path hasn't been implemented yet, raise an issue.")
+
+
+if __name__ == "__main__":
+    process_args()
--- shepherd/main.py
+++ shepherd/main.py
@@ -1,44 +1,13 @@
-import argparse
-from shepherd.utils.logger import Logger
+"""
+    This could be seen as the "interface" from Shepherd. It should essentially be an interface to all things
+    this manager can do. Including running a task, checking the status.
 
-__version__ = "v0.0.1"
-
-
-def process_args():
-
-    cmds = {
-        "version": do_version,
-        "run": do_run
-    }
-
-    parser = argparse.ArgumentParser(description="Execute a workflow")
-    subparsers = parser.add_subparsers(help="subcommand help", dest="command")
-    parser.add_argument("-d", "--debug", action="store_true")
-
-    parser_version = subparsers.add_parser("version")
-
-    parser_run = subparsers.add_parser("run")
-    parser_run.add_argument("workflow")
-    parser_run.add_argument("-i", "--inputs", help="workflow inputs")
-    parser_run.add_argument("-p", "--tools", help="required dependencies")
-    parser_run.add_argument("-e", "--environment", choices=["pmac"], default="pmac")
-    parser_run.add_argument("-E", "--engine", choices=["cromwell", "cwltool"], default="cromwell")
-
-    args = parser.parse_args()
-    print(args)
-    cmds[args.command](args)
+    NO implementation should be here. Please reference external method providers.
 
 
-def do_version(args):
-    print("v0.0.1")
+"""
 
 
-def do_run(args):
-    Logger.info("Run the shepherd-shepherd with the CommandLine arguments")
-    print(args)
+def validate_and_run_janis(wf, **kwargs):
+    pass
 
-    engine = args.engine
-
-
-if __name__ == "__main__":
-    process_args()

23e1336c57a00d09b3db6cef6e063d79afff0c82
Michael Franklin <michael@illusional.net>
2019-05-30 15:38:34 +1000

    Add dependencies into setup

--- setup.py
+++ setup.py
@@ -4,25 +4,31 @@ __version__ = "v0.0.1"
 
 modules = ["shepherd." + p for p in sorted(find_packages('./shepherd'))]
 
-setup(name="shepherd-pipelines",
-      version=__version__,
-      description='Easier way to run workflows, configurable across environments',
-      long_description=open("./README.md").read(),
-      long_description_content_type="text/markdown",
-      author='Michael Franklin',
-      author_email='michael.franklin@petermac.org',
-      license='MIT',
-      keywords=['shepherd'],
-      entry_points={
-          'console_scripts': ['shepherd=shepherd:process_args']
-      },
-      install_requires=['python-dotenv'],
-      packages=["shepherd"] + modules,
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'Topic :: Scientific/Engineering',
-          'Intended Audience :: Developers',
-          'Intended Audience :: Science/Research',
-          'Environment :: Console',
-      ],
-      )
+setup(
+    name="shepherd",
+    version=__version__,
+    description='Easier way to run workflows, configurable across environments',
+    long_description=open("./README.md").read(),
+    long_description_content_type="text/markdown",
+    author='Michael Franklin',
+    author_email='michael.franklin@petermac.org',
+    license='MIT',
+    keywords=['shepherd'],
+    entry_points={
+        'console_scripts': ['shepherd=shepherd.cli:process_args']
+    },
+    install_requires=[
+        'janis-pipelines[bioinformatics]',
+        'requests',
+        'path.py',
+        'python-dotenv'
+    ],
+    packages=["shepherd"] + modules,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Scientific/Engineering',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Environment :: Console',
+    ],
+)

49469612e3abf160e1c6261e7151b7f33d755f70
Michael Franklin <michael.franklin@petermac.org>
2019-10-09 12:08:00 +1100

    Add spartan template

---
+++ janis_runner/engines/cromwell/templates/spartan.py
@@ -0,0 +1,42 @@
+from typing import Union, List
+
+from janis_runner.engines.cromwell.cromwellconfiguration import CromwellConfiguration
+
+
+def spartan(
+    executionDir: str,
+    queues: Union[str, List[str]] = "physical",
+    email: str = None,
+    containerDir: str = None,
+    singularityVersion="3.2.0-spartan_gcc-6.2.0",
+) -> CromwellConfiguration:
+
+    if not isinstance(queues, list):
+        queues = [queues]
+
+    config = CromwellConfiguration(
+        backend=CromwellConfiguration.Backend(
+            default="slurm-spartan",
+            providers={
+                "slurm-pmac": CromwellConfiguration.Backend.Provider.slurm_singularity(
+                    singularityloadinstructions=f"module load Singularity/{singularityVersion}"
+                    + singularityVersion,
+                    singularitycontainerdir=containerDir,
+                    buildinstructions=(f"singularity pull $image docker://${{docker}}"),
+                    jobemail=email,
+                    jobqueues=queues,
+                )
+            },
+        )
+    )
+
+    backend: CromwellConfiguration.Backend.Provider.Config = config.backend.providers[
+        config.backend.default
+    ].config
+    backend.root = executionDir
+    backend.filesystems = {
+        "local": {"localization": ["cached-copy", "hard-link", "soft-link", "copy"]},
+        # "caching": {"hashing-strategy": "path+modtime"},
+    }
+
+    return config

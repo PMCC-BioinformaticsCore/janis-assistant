30f4a25a9d124cff9777184674bd40515521c8bf
Michael Franklin <michael.franklin@petermac.org>
2019-10-14 13:48:04 +1100

    Fix b

--- janis_runner/templates/base.py
+++ janis_runner/templates/base.py
@@ -6,9 +6,12 @@ from janis_runner.engines.enginetypes import EngineType
 
 
 class EnvironmentTemplate(ABC):
-    @abstractmethod
-    def engine_config(self, engine: EngineType, mail_program=None):
+    def __init__(self, mail_program=None):
         self.mail_program = mail_program
+
+    @abstractmethod
+    def engine_config(self, engine: EngineType):
+        pass
 
     def preengine_code(self, engine: EngineType):
         pass

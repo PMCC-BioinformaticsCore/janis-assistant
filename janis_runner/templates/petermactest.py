ce18cb73497414d09771e4014d46d0e432396da4
Michael Franklin <michael.franklin@petermac.org>
2019-10-22 12:36:42 +1100

    Add pmac singularity only template to test detatched submission

---
+++ janis_runner/templates/petermactest.py
@@ -0,0 +1,78 @@
+import subprocess
+from typing import Union, List
+from janis_runner.engines.enginetypes import EngineType
+from janis_runner.engines.cromwell.cromwellconfiguration import CromwellConfiguration
+from janis_runner.templates.base import EnvironmentTemplate
+
+
+class PeterMacTestTemplate(EnvironmentTemplate):
+    def __init__(
+        self,
+        executionDir: str,
+        queues: Union[str, List[str]] = "prod_med,prod",
+        containerDir="/config/binaries/singularity/containers_devel/janis/",
+        singularityVersion="3.4.0",
+    ):
+
+        super().__init__(mail_program="sendmail -t")
+        self.execution_dir = executionDir
+        self.queues = queues or ["prod_short", "prod_med", "prod"]
+        self.container_dir = containerDir
+        self.singularity_version = singularityVersion
+
+    def cromwell(self):
+
+        joined_queued = (
+            ",".join(self.queues) if isinstance(self.queues, list) else self.queues
+        )
+
+        config = CromwellConfiguration(
+            backend=CromwellConfiguration.Backend(
+                default="pmac",
+                providers={
+                    "pmac": CromwellConfiguration.Backend.Provider.singularity(
+                        singularityloadinstructions="module load singularity/"
+                        + self.singularity_version,
+                        singularitycontainerdir=self.container_dir,
+                        buildinstructions=(
+                            f"sbatch -p {joined_queued} --wait \
+                              --wrap 'docker_subbed=$(sed -e 's/[^A-Za-z0-9._-]/_/g' <<< ${{docker}}) "
+                            f"&& image={self.container_dir}/$docker_subbed.sif && singularity pull $image docker://${{docker}}'"
+                        ),
+                    )
+                },
+            )
+        )
+
+        backend: CromwellConfiguration.Backend.Provider.Config = config.backend.providers[
+            config.backend.default
+        ].config
+        backend.root = self.execution_dir
+        backend.filesystems = {
+            "local": {
+                "localization": ["cached-copy", "hard-link", "soft-link", "copy"]
+            },
+            # "caching": {"hashing-strategy": "path+modtime"},
+        }
+
+        return config
+
+    def submit_detatched_engine(self, command):
+        q = self.queues or "prod_short"
+        jq = ", ".join(q) if isinstance(q, list) else q
+        jc = " ".join(command) if isinstance(command, list) else command
+        newcommand = ["sbatch", "--queues", "--time", "30", jq, "--wrap", f"'{jc}'"]
+        subprocess.Popen(
+            newcommand,
+            close_fds=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+
+    def engine_config(self, engine: EngineType):
+        if engine == EngineType.cromwell:
+            return self.cromwell()
+
+        raise NotImplementedError(
+            f"The {self.__class__.__name__} template does not have a configuration for {engine.value}"
+        )
--- janis_runner/templates/templates.py
+++ janis_runner/templates/templates.py
@@ -1,9 +1,11 @@
 from janis_runner.templates.local import LocalTemplate
 from janis_runner.templates.petermac import PeterMacTemplate
+from janis_runner.templates.petermactest import PeterMacTestTemplate
 from janis_runner.templates.spartan import SpartanTemplate
 
 templates = {
     "local": LocalTemplate,
     "pmac": PeterMacTemplate,
     "spartan": SpartanTemplate,
+    "pmac2": PeterMacTestTemplate,
 }

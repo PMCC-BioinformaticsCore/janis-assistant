2e0853984ec6d03839a4655b615b62c29d55a0ed
Michael Franklin <michael.franklin@petermac.org>
2019-10-31 09:12:08 +1100

    Add default "Docker" container type to template

--- janis_runner/templates/base.py
+++ janis_runner/templates/base.py
@@ -1,13 +1,17 @@
 from abc import ABC, abstractmethod
+from typing import Type
 
 from janis_core import Logger
 
+from janis_runner.containers.base import Container
+from janis_runner.containers.docker import Docker
 from janis_runner.engines.enginetypes import EngineType
 
 
 class EnvironmentTemplate(ABC):
-    def __init__(self, mail_program=None):
+    def __init__(self, mail_program=None, containertype: Type[Container] = Docker):
         self._mail_program = mail_program
+        self.containertype = containertype
 
     @abstractmethod
     def engine_config(self, engine: EngineType):
@@ -28,3 +32,5 @@ class EnvironmentTemplate(ABC):
     def post_configuration_hook(self, configuration):
         if self._mail_program:
             configuration.notifications.mail_program = self._mail_program
+
+        configuration.container = self.containertype

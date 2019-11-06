cbd74a0603e641c14402a869e1634ada5d632658
Michael Franklin <michael.franklin@petermac.org>
2019-11-06 15:40:36 +1100

    Bump version for release

--- janis_runner/__meta__.py
+++ janis_runner/__meta__.py
@@ -1,3 +1,3 @@
-__version__ = "v0.7.1"
+__version__ = "v0.7.2"
 GITHUB_URL = "https://github.com/PMCC-BioinformaticsCore/janis-runner"
 ISSUE_URL = "https://github.com/PMCC-BioinformaticsCore/janis-runner/issues/new"

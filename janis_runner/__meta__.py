eb1f78c34133ec211f4562c3fb3ca0a47735c1a4
Michael Franklin <michael.franklin@petermac.org>
2019-11-07 12:18:20 +1100

    Add __init__ and repeg for release

--- janis_runner/__meta__.py
+++ janis_runner/__meta__.py
@@ -1,3 +1,3 @@
-__version__ = "v0.7.3"
+__version__ = "v0.7.4"
 GITHUB_URL = "https://github.com/PMCC-BioinformaticsCore/janis-runner"
 ISSUE_URL = "https://github.com/PMCC-BioinformaticsCore/janis-runner/issues/new"

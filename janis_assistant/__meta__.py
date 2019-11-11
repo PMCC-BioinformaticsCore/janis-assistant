f23ee5907433f64adc6cd04ce19f6703e217118b
Michael Franklin <michael.franklin@petermac.org>
2019-11-11 11:01:04 +1100

    Retag for release

--- janis_assistant/__meta__.py
+++ janis_assistant/__meta__.py
@@ -1,3 +1,3 @@
-__version__ = "v0.7.6"
+__version__ = "v0.7.7"
 GITHUB_URL = "https://github.com/PMCC-BioinformaticsCore/janis-assistant"
 ISSUE_URL = "https://github.com/PMCC-BioinformaticsCore/janis-assistant/issues/new"

d287b7ddf6e3991c586bd8d32a6f8ae9613dd1ec
Michael Franklin <michael.franklin@petermac.org>
2019-11-15 15:17:08 +1100

    Bump version for release

--- janis_assistant/__meta__.py
+++ janis_assistant/__meta__.py
@@ -1,3 +1,3 @@
-__version__ = "v0.7.10"
+__version__ = "v0.7.11"
 GITHUB_URL = "https://github.com/PMCC-BioinformaticsCore/janis-assistant"
 ISSUE_URL = "https://github.com/PMCC-BioinformaticsCore/janis-assistant/issues/new"

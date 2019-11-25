b1a1c8a124ec24be8ff5989d203bd149c9585246
Michael Franklin <michael.franklin@petermac.org>
2019-11-25 15:23:59 +1100

    Add more options to JanisTemplate

--- janis_assistant/templates/base.py
+++ janis_assistant/templates/base.py
@@ -10,9 +10,17 @@ from janis_assistant.engines.enginetypes import EngineType
 
 
 class EnvironmentTemplate(ABC):
-    def __init__(self, mail_program=None, containertype: Type[Container] = Docker):
+    def __init__(
+        self,
+        mail_program=None,
+        max_cores=None,
+        max_ram=None,
+        containertype: Type[Container] = Docker,
+    ):
         self._mail_program = mail_program
         self.containertype = containertype
+        self.max_cores = max_cores
+        self.max_ram = max_ram
 
     @abstractmethod
     def engine_config(self, engine: EngineType):
@@ -28,11 +36,16 @@ class EnvironmentTemplate(ABC):
             stdout=subprocess.DEVNULL,
             stderr=subprocess.DEVNULL,
         )
-        # subprocess.Popen(command) # Keep process open
 
     def post_configuration_hook(self, configuration):
         if self._mail_program:
             configuration.notifications.mail_program = self._mail_program
+
+        if self.max_cores and not configuration.environment.max_cores:
+            configuration.environment.max_cores = self.max_cores
+
+        if self.max_ram and not configuration.environment.max_ram:
+            configuration.environment.max_ram = self.max_ram
 
         configuration.container = self.containertype
 
@@ -44,13 +57,20 @@ class SingularityEnvironmentTemplate(EnvironmentTemplate):
         containerDir: str,
         loadInstructions=None,
         buildInstructions=f"singularity pull $image docker://${{docker}}",
+        max_cores=None,
+        max_ram=None,
     ):
-        super().__init__(mail_program=mail_program, containertype=Singularity)
+        super().__init__(
+            mail_program=mail_program,
+            containertype=Singularity,
+            max_cores=max_cores,
+            max_ram=max_ram,
+        )
         self.singularity_load_instructions = loadInstructions
         self.singularity_container_dir = containerDir
         self.singularity_build_instructions = buildInstructions
 
-        Logger.info(
+        Logger.log(
             f"Setting Singularity: containerdir={containerDir}, loadinstructions={loadInstructions}"
         )
         # little bit hacky

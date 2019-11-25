c14540379eff1672fdcbf8f19681c3ed3c890f18
Michael Franklin <michael.franklin@petermac.org>
2019-11-25 15:45:35 +1100

    Add pasey, singularity, slurm-singularity templates

--- janis_assistant/templates/templates.py
+++ janis_assistant/templates/templates.py
@@ -1,11 +1,18 @@
-from janis_assistant.templates.local import LocalTemplate
+from janis_assistant.templates.local import LocalTemplate, LocalSingularityTemplate
+from janis_assistant.templates.pawsey import PawseyDisconnectedTemplate
 from janis_assistant.templates.petermac import PeterMacTemplate
 from janis_assistant.templates.petermacdisconnected import PeterMacDisconnectedTemplate
+from janis_assistant.templates.slurm import SlurmSingularityTemplate
 from janis_assistant.templates.spartan import SpartanTemplate
 
 templates = {
+    # generic templates
     "local": LocalTemplate,
+    "singularity": LocalSingularityTemplate,
+    "slurm_singularity": SlurmSingularityTemplate,
+    # location specific templates
     "pmac": PeterMacTemplate,
     "spartan": SpartanTemplate,
     "pmac-disconnected": PeterMacDisconnectedTemplate,
+    "pawsey": PawseyDisconnectedTemplate,
 }

00fafead66541b964506ea34a072fb269a16dddf
Michael Franklin <michael.franklin@petermac.org>
2019-11-25 14:59:09 +1100

    Cleanup pmac disconnected to inherit from petermac base

--- janis_assistant/templates/petermacdisconnected.py
+++ janis_assistant/templates/petermacdisconnected.py
@@ -3,9 +3,6 @@ from typing import Union, List
 
 from janis_core import Logger
 
-from janis_assistant.engines.enginetypes import EngineType
-from janis_assistant.engines.cromwell.cromwellconfiguration import CromwellConfiguration
-from janis_assistant.templates.base import EnvironmentTemplate
 from janis_assistant.templates.petermac import PeterMacTemplate
 
 
@@ -19,50 +16,19 @@ class PeterMacDisconnectedTemplate(PeterMacTemplate):
         catchSlurmErrors=True,
     ):
 
+        buildinstructions = (
+            f"unset SINGULARITY_TMPDIR && docker_subbed=$(sed -e 's/[^A-Za-z0-9._-]/_/g' <<< ${{docker}}) "
+            f"&& image={containerDir}/$docker_subbed.sif && singularity pull $image docker://${{docker}}"
+        )
+
         super().__init__(
             executionDir=executionDir,
             queues=queues,
             containerDir=containerDir,
             singularityVersion=singularityVersion,
             catchSlurmErrors=catchSlurmErrors,
+            singularityBuildInstructions=buildinstructions,
         )
-
-    def cromwell(self):
-
-        # This will be changed to: queues = "janis" once partition as been created
-        joined_queued = (
-            ",".join(self.queues) if isinstance(self.queues, list) else self.queues
-        )
-
-        config = CromwellConfiguration(
-            backend=CromwellConfiguration.Backend(
-                default="pmac",
-                providers={
-                    "pmac": CromwellConfiguration.Backend.Provider.slurm_singularity(
-                        singularityloadinstructions="module load singularity/"
-                        + self.singularity_version,
-                        singularitycontainerdir=self.container_dir,
-                        buildinstructions=(
-                            f"unset SINGULARITY_TMPDIR && docker_subbed=$(sed -e 's/[^A-Za-z0-9._-]/_/g' <<< ${{docker}}) && "
-                            f"image={self.container_dir}/$docker_subbed.sif && singularity pull $image docker://${{docker}}"
-                        ),
-                        jobemail=None,
-                        jobqueues=joined_queued,
-                        afternotokaycatch=self.catch_slurm_errors,
-                    )
-                },
-            )
-        )
-
-        backend: CromwellConfiguration.Backend.Provider.Config = config.backend.providers[
-            config.backend.default
-        ].config
-        backend.root = self.execution_dir
-        backend.filesystems = {
-            "local": {"localization": ["cached-copy", "hard-link", "soft-link", "copy"]}
-        }
-
-        return config
 
     def submit_detatched_resume(self, wid, command):
         q = self.queues or "prod_short"
@@ -88,11 +54,3 @@ class PeterMacDisconnectedTemplate(PeterMacTemplate):
         )
         if rc != 0:
             raise Exception(f"Couldn't submit janis-monitor, non-zero exit code ({rc})")
-
-    def engine_config(self, engine: EngineType):
-        if engine == EngineType.cromwell:
-            return self.cromwell()
-
-        raise NotImplementedError(
-            f"The {self.__class__.__name__} template does not have a configuration for {engine.value}"
-        )

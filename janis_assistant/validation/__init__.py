dca87169032fdd4cb30070bee912ebab20d81437
Michael Franklin <michael.franklin@petermac.org>
2019-11-26 12:16:06 +1100

    Fix validator helper

--- janis_assistant/validation/__init__.py
+++ janis_assistant/validation/__init__.py
@@ -1,6 +1,7 @@
 from typing import List, Dict, Optional
 
-from janis_core import Tool, Workflow, WorkflowBuilder, DataType, ToolOutput, Logger
+from janis_core import Tool, DataType
+from janis_core.tool.tool import TOutput
 
 
 class ValidationRequirements:
@@ -20,14 +21,14 @@ class ValidationRequirements:
 def ensure_outputs_are_in_workflow_and_are_compatible(
     tool: Tool, outputs: List[str], compatible_type: DataType
 ):
-    tool_outputs: Dict[str, ToolOutput] = tool.outputs_map()
+    tool_outputs: Dict[str, TOutput] = tool.outputs_map()
     failed_outputs = []
     untyped_outputs = []
 
     for o in outputs:
         if o not in tool_outputs:
             failed_outputs.append(o)
-        elif not compatible_type.can_receive_from(tool_outputs[o].output_type):
+        elif not compatible_type.can_receive_from(tool_outputs[o].outtype):
             untyped_outputs.append(o)
 
     return failed_outputs, untyped_outputs

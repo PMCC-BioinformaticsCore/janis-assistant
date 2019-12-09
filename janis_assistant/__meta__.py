1a927269ba0892c533322061929e6d3821c7b0ba
Michael Franklin <michael.franklin@petermac.org>
2019-12-09 14:40:41 +1100

    Bump to 0.7 version (unwind 0.8)

--- janis_assistant/__meta__.py
+++ janis_assistant/__meta__.py
@@ -1,4 +1,4 @@
-__version__ = "v0.8.0"
+__version__ = "v0.7.16"
 DOCS_URL = "https://janis.readthedocs.io"
 GITHUB_URL = "https://github.com/PMCC-BioinformaticsCore/janis-assistant"
 ISSUE_URL = "https://github.com/PMCC-BioinformaticsCore/janis-assistant/issues/new"

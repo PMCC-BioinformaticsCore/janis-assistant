fe880963e33061fa48941e4840fb6f0a7c0c7f06
Michael Franklin <michael.franklin@petermac.org>
2020-02-07 09:20:14 +1100

    Fixes an issue where array of files would not correctly qualify filepath

--- janis_assistant/modifiers/inputqualifier.py
+++ janis_assistant/modifiers/inputqualifier.py
@@ -1,6 +1,6 @@
 from typing import Dict
 
-from janis_core import Tool, File
+from janis_core import Tool, File, Array
 
 from janis_assistant.modifiers.base import PipelineModifierBase
 from janis_assistant.utils import fully_qualify_filename
@@ -13,9 +13,24 @@ class InputFileQualifierModifier(PipelineModifierBase):
         for tag, value in nin.items():
             if tag not in inmap:
                 continue
-            if not isinstance(inmap[tag].intype.received_type(), File):
+            intype = inmap[tag].intype
+
+            isfiletype = isinstance(intype.received_type(), File)
+            isbasefiletype = isinstance(intype.received_type(), Array) and isinstance(
+                intype.fundamental_type(), File
+            )
+            if not (isfiletype or isbasefiletype):
                 continue
 
-            nin[tag] = fully_qualify_filename(value)
+            nin[tag] = self.fully_qualify_filename_array_or_single(value)
 
         return nin
+
+    @staticmethod
+    def fully_qualify_filename_array_or_single(value):
+        if isinstance(value, list):
+            return [
+                InputFileQualifierModifier.fully_qualify_filename_array_or_single(t)
+                for t in value
+            ]
+        return fully_qualify_filename(value)

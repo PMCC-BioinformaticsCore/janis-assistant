879e1a3987bd0c07faad07049bd20c3d36535b3d
Michael Franklin <michael.franklin@petermac.org>
2020-02-07 11:50:13 +1100

    Better document InputQualifier

--- janis_assistant/modifiers/inputqualifier.py
+++ janis_assistant/modifiers/inputqualifier.py
@@ -1,33 +1,70 @@
-from typing import Dict
+from typing import Dict, Union, List
 
-from janis_core import Tool, File, Array
+from os import getcwd
+from path import Path
 
+from janis_core import Tool, File, Array, Directory
 from janis_assistant.modifiers.base import PipelineModifierBase
 from janis_assistant.utils import fully_qualify_filename
 
 
 class InputFileQualifierModifier(PipelineModifierBase):
+    """
+    InputFileQualifier is designed to take relative file paths from the CWD and turn
+    them into fully qualified paths. It also expands path components like '~' or '.'.
+
+    The InputFileQualifier allows you to override the 'cwd' during the init, otherwise
+    the current cwd is captured on initialise.
+    """
+
+    def __init__(self, cwd=None):
+        """
+        The current working directory from which to capture relative paths
+        :param cwd:
+        """
+        super().__init__()
+        self.cwd = cwd or getcwd()
+
     def inputs_modifier(self, wf: Tool, inputs: Dict, hints: Dict[str, str]):
         nin = {**inputs}
         inmap = wf.inputs_map()
-        for tag, value in nin.items():
-            if tag not in inmap:
-                continue
-            intype = inmap[tag].intype
 
-            isfiletype = isinstance(intype.received_type(), File)
-            isbasefiletype = isinstance(intype.received_type(), Array) and isinstance(
-                intype.fundamental_type(), File
-            )
-            if not (isfiletype or isbasefiletype):
-                continue
+        # Change the 'cwd' just for the scope of this block
+        with Path(self.cwd):
+            for tag, value in nin.items():
+                if tag not in inmap:
+                    # Only localise tags within the inputsdict that are in the tool inputs
+                    # This might be a problem later for passthrough inputs (Janis doesn't support yet)
+                    continue
+                intype = inmap[tag].intype
 
-            nin[tag] = self.fully_qualify_filename_array_or_single(value)
+                # If the type of the input is:
+                #   (a) A File / Directory
+                isfiletype = isinstance(intype.received_type(), (File, Directory))
 
-        return nin
+                #   (b) Array with a fundamental type (recursively searches if nested array) of File or Directory
+                isbasefiletype = isinstance(
+                    intype.received_type(), Array
+                ) and isinstance(intype.fundamental_type(), (File, Directory))
+
+                # (Skip if not one of these conditions)
+                if not (isfiletype or isbasefiletype):
+                    continue
+
+                # Qualify the fully qualify the filepath
+                nin[tag] = self.fully_qualify_filename_array_or_single(value)
+
+            return nin
 
     @staticmethod
-    def fully_qualify_filename_array_or_single(value):
+    def fully_qualify_filename_array_or_single(
+        value: Union[str, List[str], List[List[str]]]
+    ):
+        """
+
+        :param value:
+        :return:
+        """
         if isinstance(value, list):
             return [
                 InputFileQualifierModifier.fully_qualify_filename_array_or_single(t)

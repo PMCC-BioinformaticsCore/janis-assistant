0ca62cd3c7f64deccdacadf5c31f5638eac550ee
Michael Franklin <michael.franklin@petermac.org>
2020-04-14 14:56:34 +1000

    Add "suspended" state

--- janis_assistant/data/enums/taskstatus.py
+++ janis_assistant/data/enums/taskstatus.py
@@ -23,6 +23,7 @@ class TaskStatus(Enum):
             TaskStatus.FAILED,
             TaskStatus.ABORTED,
             TaskStatus.DRY_RUN,
+            TaskStatus.SUSPENDED,
         ]
 
     def __str__(self):
@@ -35,6 +36,7 @@ class TaskStatus(Enum):
             TaskStatus.ABORTED.value: "Terminated",
             TaskStatus.DRY_RUN.value: "Dry run",
             TaskStatus.ON_HOLD.value: "On hold",
+            TaskStatus.SUSPENDED.value: "Suspended",
         }
         return __str[self.value]
 
@@ -48,5 +50,6 @@ class TaskStatus(Enum):
             TaskStatus.ABORTED.value: "T",
             TaskStatus.DRY_RUN.value: "DR",
             TaskStatus.ON_HOLD.value: "?",
+            TaskStatus.SUSPENDED.value: "II",
         }
         return __str[self.value]

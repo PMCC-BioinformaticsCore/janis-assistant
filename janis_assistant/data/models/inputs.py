c1e6288bb4b666ab4c6b6058010d4d3bf99929bb
Michael Franklin <michael.franklin@petermac.org>
2020-06-22 12:14:53 +1000

    Add inputDB fix

--- janis_assistant/data/models/inputs.py
+++ janis_assistant/data/models/inputs.py
@@ -1,4 +1,5 @@
 import json
+from janis_core.utils.logger import Logger
 
 
 class WorkflowInputModel:
@@ -11,4 +12,12 @@ class WorkflowInputModel:
 
     @staticmethod
     def from_row(row):
-        return WorkflowInputModel(row[1], json.loads(row[2]))
+        try:
+            return WorkflowInputModel(row[1], json.loads(row[2]))
+        except TypeError as e:
+            # I've seen this happen even if you write a string to the database for some reason
+            Logger.debug(f"Couldn't parse json value from row: {str(row)}: {repr(e)}")
+            return WorkflowInputModel(row[1], row[2])
+
+    def __repr__(self):
+        return str((self.tag, self.value))

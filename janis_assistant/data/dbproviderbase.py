59188a458f874e20ed8dbece4feef11bc47da3bc
Michael Franklin <michael.franklin@petermac.org>
2020-06-30 14:30:56 +1000

    Update DbProviderBase with better get

--- janis_assistant/data/dbproviderbase.py
+++ janis_assistant/data/dbproviderbase.py
@@ -1,12 +1,26 @@
-from typing import Type, List, Tuple, Dict, Union
+from typing import (
+    Type,
+    List,
+    Tuple,
+    Dict,
+    Union,
+    ContextManager,
+    Optional,
+    TypeVar,
+    Generic,
+)
 from abc import abstractmethod
-from sqlite3 import Connection
+from sqlite3 import Connection, Cursor, OperationalError
 from contextlib import contextmanager
+
+from janis_core import Logger
 
 from janis_assistant.data.models.base import DatabaseObject
 
+T = TypeVar("T")
 
-class DbProviderBase:
+
+class DbProviderBase(Generic[T]):
     def __init__(
         self,
         base_type: Type[DatabaseObject],
@@ -25,7 +39,7 @@ class DbProviderBase:
             cursor.execute(schema)
 
     @contextmanager
-    def with_cursor(self):
+    def with_cursor(self) -> ContextManager[Cursor]:
         cursor = None
         try:
             cursor = self.db.cursor()
@@ -37,7 +51,7 @@ class DbProviderBase:
 
     def get(
         self, keys: Union[str, List[str]] = "*", where: Tuple[str, List[any]] = None
-    ):
+    ) -> Optional[List[T]]:
         jkeys = ", ".join(keys) if isinstance(keys, list) else keys
         if jkeys == "*":
             keys = [k for _, k in self.base.table_schema()]
@@ -59,7 +73,20 @@ class DbProviderBase:
             query += f" WHERE {' AND '.join(whereclauses)}"
 
         with self.with_cursor() as cursor:
-            rows = cursor.execute(query, values)
+            try:
+                rows = cursor.execute(query, values).fetchall()
+            except OperationalError as e:
+                if "readonly database" in str(e):
+                    # mfranklin: idk, this sometimes happens. We're doing a select query, idk sqlite3 driver...
+                    Logger.debug(
+                        "Got readonly error when running query: '{query}', skipping for now"
+                    )
+                    return None
+                elif "locked" in str(e):
+                    Logger.debug(
+                        "We hit the database at the same time the janis process wrote to it, meh"
+                    )
+                    return None
 
         parsedrows = [self.base.deserialize(keys, r) for r in rows]
         return parsedrows
@@ -73,3 +100,28 @@ class DbProviderBase:
         {self.base.table_schema()}
         )
         """
+
+    def insert_or_update_many(self, els: List[T]):
+        queries: Dict[str, List[List[any]]] = {}
+        update_separator = ",\n"
+        tab = "\t"
+        for el in els:
+            keys, values = el.prepare_insert()
+            prepared_statement = f"""
+INSERT INTO {self.tablename}
+    ({', '.join(keys)})
+VALUES
+    ({', '.join(f':{k}' for k in keys)})
+ON DUPLICATE KEY UPDATE 
+{update_separator.join(f'{tab}{k} = :{k}' for k in keys)};
+"""
+            if prepared_statement in queries:
+                queries[prepared_statement].append(values)
+            else:
+                queries[prepared_statement] = [values]
+
+        with self.with_cursor() as cursor:
+            for query, vvalues in queries.items():
+                cursor.executemany(query, vvalues)
+
+        return True

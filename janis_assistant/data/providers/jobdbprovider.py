246c9de6caa1c578e119a6de3f693a06acd01e6c
Michael Franklin <michael.franklin@petermac.org>
2020-08-06 18:58:54 +1000

    Better RunJobModel filtering

--- janis_assistant/data/providers/jobdbprovider.py
+++ janis_assistant/data/providers/jobdbprovider.py
@@ -1,6 +1,8 @@
+from sys import getsizeof
 from sqlite3 import OperationalError
-from typing import List, TypeVar, Iterable, Union, Callable, Dict, Optional
+from typing import List, TypeVar, Iterable, Union, Callable, Dict, Optional, Tuple
 
+from janis_assistant.data.enums import TaskStatus
 from janis_core import Logger
 
 from janis_assistant.data.dbproviderbase import DbProviderBase
@@ -29,6 +31,8 @@ def groupby(
 
 
 class JobDbProvider(DbProviderBase):
+    job_cache_warnings = [10, 100, 500, 1000]
+
     def __init__(self, db, readonly, submission_id):
         super().__init__(
             base_type=RunJobModel,
@@ -38,6 +42,13 @@ class JobDbProvider(DbProviderBase):
             scopes={"submission_id": submission_id},
         )
         self.submission_id = submission_id
+        self.job_cache_last_idx = 0
+
+        """
+        We seem to persistently have problems with updating the DB, we can't use
+        newer versions of SQLite because of a stakeholder issue.
+        """
+        self._cache_completed_ids: Dict[Tuple, str] = {}
 
     def get_single(
         self, id_: str, run_id: str = RunModel.DEFAULT_ID
@@ -102,6 +113,76 @@ class JobDbProvider(DbProviderBase):
             # job.events = groupedevents.get(job.jid, [])
 
         return [j for j in alljobs if j.parent is None]
+
+    ## FILTERING
+
+    def populate_cache_if_required(self):
+        if self._cache_completed_ids is not None:
+            return False
+        self.populate_cache()
+        return True
+
+    def populate_cache(self):
+        self._cache_completed_ids = {}
+        idkeys = set(self.get_id_keys())
+        idkeys_ordered = list(idkeys)
+        prows = f"SELECT {', '.join(idkeys_ordered)}, status FROM {self._tablename}"
+        with self.with_cursor() as cursor:
+            rows = cursor.execute(prows).fetchall()
+
+            if len(rows) == 0:
+                return
+
+            for row in rows:
+                idkeys = tuple(row[:-2])
+                # is_updateable = not TaskStatus(row[-1]).is_in_final_state()
+                self._cache_completed_ids[idkeys] = row[-1]
+
+    def filter_updates(
+        self, jobs: List[RunJobModel], add_inserts_to_cache=True
+    ) -> Tuple[List[RunJobModel], List[RunJobModel]]:
+        # don't call super, it'll break because of the cache
+
+        updates = []
+        inserts = []
+
+        if len(jobs) == 0:
+            return updates, inserts
+
+        self.populate_cache_if_required()
+
+        idkeys = set(self.get_id_keys())
+        idkeys_ordered = list(idkeys)
+        dbalias_map = {t.dbalias: t.name for t in self._base.keymap()}
+        skipped = 0
+
+        for job in jobs:
+            el_idkey = tuple([getattr(job, dbalias_map[_k]) for _k in idkeys_ordered])
+
+            jstatus = self._cache_completed_ids.get(el_idkey)
+            if jstatus is None:
+                inserts.append(job)
+            elif job.status.value != jstatus:
+                updates.append(job)
+            elif jstatus:
+                skipped += 1
+            self._cache_completed_ids[el_idkey] = job.status.value
+
+        if skipped:
+            Logger.log(
+                f"Skipped updating {skipped} jobs as those jobs were already in a final state"
+            )
+        memory = getsizeof(self._cache_completed_ids) // 1024
+        if (
+            self.job_cache_last_idx < len(self.job_cache_warnings)
+            and memory > self.job_cache_warnings[self.job_cache_last_idx]
+        ):
+            Logger.warn(f"Job cache is using {memory} MB")
+            self.job_cache_last_idx += 1
+
+        return updates, inserts
+
+        # We'll remove all updates
 
     def upgrade_schema(self, from_version: int):
         # if from_version < 2:

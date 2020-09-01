e5b01bbdbba3ea88e808f77a064621b1373989d9
Michael Franklin <michael.franklin@petermac.org>
2020-09-01 17:21:32 +1000

    Bump min deps of janis-core

--- setup.py
+++ setup.py
@@ -26,8 +26,8 @@ setup(
         "janis.extension": ["assistant=janis_assistant"],
     },
     install_requires=[
-        "janis-pipelines.core>=0.10.2",
-        "janis-pipelines.templates>=0.10.1",
+        "janis-pipelines.core>=0.10.4",
+        "janis-pipelines.templates>=0.10.2",
         "requests",
         "path",
         "python-dateutil",

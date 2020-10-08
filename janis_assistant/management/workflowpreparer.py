fcb61e3a7f729d20632392e2d5163deba47d907e
Michael Franklin <michael.franklin@petermac.org>
2020-10-08 18:51:28 +1100

    Guess filetype and auto determine transformation back to the expected

--- janis_assistant/management/workflowpreparer.py
+++ janis_assistant/management/workflowpreparer.py
@@ -1,7 +1,15 @@
 from contextlib import contextmanager
 from typing import Dict, Set, Optional
 
-from janis_core import Workflow, Logger, Array
+from janis_core import (
+    Workflow,
+    Logger,
+    Array,
+    File,
+    JanisShed,
+    JanisTransformationGraph,
+    JanisTransformation,
+)
 
 # we need to localise a FASTA reference
 from janis_core.workflow.workflow import InputNode
@@ -25,7 +33,7 @@ def check_if_inputs_with_examples_are_present(tool: Workflow, inputs, hints):
 
     for inpnode in tool.input_nodes.values():
         if inpnode.id() in inputs:
-            continue
+            check_input_for_correctness(inpnode, inputs[inpnode.id()])
         elif inpnode.default is not None or inpnode.datatype.optional:
             continue
 
@@ -42,6 +50,63 @@ def check_if_inputs_with_examples_are_present(tool: Workflow, inputs, hints):
 
     if missing:
         Logger.warn("There are missing inputs: " + ", ".join(missing))
+
+
+def check_input_for_correctness(inp: InputNode, value: any):
+    if not isinstance(inp.datatype, File):
+        return
+
+    if not isinstance(value, str):
+        Logger.warn(
+            f"Expecting string type input '{inp.id()}' of file File, but received '{type(value)}'"
+        )
+
+    # check extension (and in future, secondaries)
+    extensions = {
+        inp.datatype.extension,
+        *list(inp.datatype.additional_extensions or []),
+    }
+    has_extension = False
+    for ext in extensions:
+        if value.endswith(ext):
+            has_extension = True
+            break
+
+    if has_extension:
+        # looks like we're sweet
+        Logger.debug(
+            f"Validated that the input for {inp.id()} had the expected extension for {inp.datatype.id()}"
+        )
+        return
+
+    guessed_datatype = JanisShed.guess_datatype_by_filename(value)
+    message_prefix = f"The value for input '{inp.id()}' did not match the expected extension for {inp.datatype.name()} (expected: {', '.join(extensions)})"
+    if not guessed_datatype:
+        Logger.warn(
+            message_prefix
+            + f"\nand Janis couldn't guess the datatype from the input for {inp.id()} and value '{value}'."
+        )
+    try:
+        transformation = JanisShed.get_transformation_graph().find_connection(
+            guessed_datatype, inp.datatype
+        )
+        steps = (
+            "".join(t.type1.name() + " -> " for t in transformation)
+            + transformation[-1].type2.name()
+        )
+        Logger.warn(
+            message_prefix
+            + f",\nJanis guessed the actual datatype for '{inp.id()}' from data '{value}' to be {guessed_datatype.id()}, \n"
+            f"and Janis was able to determine a transformation in {len(transformation)} step(s): {steps}"
+        )
+        JanisTransformation.convert_transformations_to_workflow(
+            transformation
+        ).translate("wdl")
+    except:
+        Logger.warn(
+            message_prefix
+            + f",\nbut Janis couldn't find a transformation between the guessed and expected type: {guessed_datatype.id()} -> {inp.datatype.id()}"
+        )
 
 
 def find_fasta_files(inp: InputNode):

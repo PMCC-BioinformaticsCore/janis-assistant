f832050c90bf5bf73cab9a6a45275b499e95f8b3
Michael Franklin <michael.franklin@petermac.org>
2020-10-15 16:19:45 +1100

    Add stub for FileFinder

---
+++ janis_assistant/modifiers/filefinder.py
@@ -0,0 +1,31 @@
+from typing import Dict
+
+from janis_core import Tool, WorkflowBase
+
+from janis_assistant.modifiers.base import PipelineModifierBase
+
+
+class FileFinder(PipelineModifierBase):
+    def __init__(self, cache_dir: str):
+        self.cache_dir = cache_dir
+
+    # def inputs_modifier(self, tool: Tool, inputs: Dict, hints: Dict[str, str]) -> Dict:
+    #
+    #     if not isinstance(tool, WorkflowBase):
+    #         return inputs
+    #
+    #     wf: WorkflowBase = tool
+    #
+    #     for inpnode in wf.input_nodes.values():
+    #         if inpnode.id() in inputs and inputs[inpnode.id()] is not None:
+    #             continue
+    #         elif inpnode.default is not None or inpnode.datatype.optional:
+    #             continue
+    #
+    #         if inpnode.doc.example:
+    #
+    #
+    #     elif isinstance(inpnode.datatype, Fasta):
+    #     find_fasta_files(inpnode)
+    #
+    #     return inputs

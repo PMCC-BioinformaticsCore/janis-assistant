e2ce18bd7b8a66fe4041b6cde4476f466ecac396
Juny Kesumadewi <juny.kesumadewi@unimelb.edu.au>
2021-04-26 21:05:07 +1000

    run prejanis hook

--- janis_assistant/templates/base.py
+++ janis_assistant/templates/base.py
@@ -78,6 +78,9 @@ class EnvironmentTemplate(ABC):
     ):
         import subprocess
 
+        if self.prejanis_hook() is not None:
+            command = f"{self.prejanis_hook()}; {command}"
+
         Logger.info(
             "Starting Janis in the background with: "
             + (

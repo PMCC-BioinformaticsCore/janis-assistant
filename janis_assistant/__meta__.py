1a5e8167152c0053bc4033c670deb0bfe8fbe5b2
Grace Hall <graceavahall@gmail.com>
2023-01-31 12:34:28 +1100

    github release version

--- janis_assistant/__meta__.py
+++ janis_assistant/__meta__.py
@@ -1,4 +1,4 @@
-__version__ = "v0.11.7"
+__version__ = "v0.12.0.2"
 DOCS_URL = "https://janis.readthedocs.io"
 GITHUB_URL = "https://github.com/PMCC-BioinformaticsCore/janis-assistant"
 ISSUE_URL = "https://github.com/PMCC-BioinformaticsCore/janis-assistant/issues/new"
